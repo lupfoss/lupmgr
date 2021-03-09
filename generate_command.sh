e203bfb2e144fb38d3a229f182da94a461818640
Manu Bansal <manub@lightup.ai>
2021-03-09 06:19:33 +0000

    support for rc local service add

---
+++ generate_command.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source user_config.sh
+source fixed_config.sh
+
+this_dir=$(pwd)
+
+echo  \
+"AUTOSSH_DEBUG=1 \
+AUTOSSH_LOGFILE=${this_dir}/autossh.log \
+autossh -f -M 0 ${LIGHTUP_CONNECT_SERVER_NAME} -p ${LIGHTUP_CONNECT_SERVER_PORT} -N \
+	-o ExitOnForwardFailure=yes \
+	-o UserKnownHostsFile=/dev/null \
+	-o StrictHostKeyChecking=no \
+	-R 9000:localhost:22 -vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}"
