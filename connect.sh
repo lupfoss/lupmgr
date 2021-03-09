051aebc2ed221d23e32c733bc834801ac627c2dc
Manu Bansal <manub@lightup.ai>
2021-03-09 05:25:03 +0000

    more options to autossh

--- connect.sh
+++ connect.sh
@@ -7,7 +7,11 @@ this_dir=$(pwd)
 
 AUTOSSH_DEBUG=1 \
 AUTOSSH_LOGFILE="autossh.log" \
-autossh -f -M 0 ${LIGHTUP_CONNECT_SERVER_NAME} -p ${LIGHTUP_CONNECT_SERVER_PORT} -N -o ExitOnForwardFailure=yes -R 9000:localhost:22 -vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}
+autossh -f -M 0 ${LIGHTUP_CONNECT_SERVER_NAME} -p ${LIGHTUP_CONNECT_SERVER_PORT} -N \
+	-o ExitOnForwardFailure=yes \
+	-o UserKnownHostsFile=/dev/null \
+	-o StrictHostKeyChecking=no \
+	-R 9000:localhost:22 -vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}
 
 err=$?
 
