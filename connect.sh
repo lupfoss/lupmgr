5dc48e9a0b5aaf2c49c571c81500f001f13b9c4a
Manu Bansal <manub@lightup.ai>
2021-03-09 06:24:32 +0000

    code movearound

--- connect.sh
+++ connect.sh
@@ -5,17 +5,10 @@ source fixed_config.sh
 
 this_dir=$(pwd)
 
-#AUTOSSH_DEBUG=1 \
-#AUTOSSH_LOGFILE=${this_dir}/autossh.log \
-#autossh -f -M 0 ${LIGHTUP_CONNECT_SERVER_NAME} -p ${LIGHTUP_CONNECT_SERVER_PORT} -N \
-#	-o ExitOnForwardFailure=yes \
-#	-o UserKnownHostsFile=/dev/null \
-#	-o StrictHostKeyChecking=no \
-#	-R 9000:localhost:22 -vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}
-
-./generate_command.sh > connect_command.sh
-chmod +x connect_command.sh
-./connect_command.sh
+mkdir -p generated
+./generate_command.sh > generated/connect_command.sh
+chmod +x generated/connect_command.sh
+./generated/connect_command.sh
 
 err=$?
 
--- setup_part_two.sh
+++ setup_part_two.sh
@@ -3,8 +3,18 @@
 source user_config.sh
 source fixed_config.sh
 
+# key setup
 echo "adding Lightup's public key to authorized keys..."
 scp -i ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} -P ${LIGHTUP_CONNECT_SERVER_PORT} ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME}:~/"${LIGHTUP_ACCEPT_KEYPAIR_NAME}.pub" ./keys/
 mkdir -p ~/.ssh && cat "./keys/${LIGHTUP_ACCEPT_KEYPAIR_NAME}.pub" >> ~/.ssh/authorized_keys
 
-
+# rc.local setup
+mkdir -p generated/
+sudo cp rc-local.service /etc/systemd/system/rc-local.service
+echo "#!/usr/bin/env bash" > generated/rc.local.tmp
+./generate_command.sh >> generated/rc.local.tmp
+sudo cp generated/rc.local.tmp /etc/rc.local
+sudo chmod +x /etc/rc.local
+sudo systemctl enable rc-local
+sudo systemctl start rc-local.service
+sudo systemctl status rc-local.service
