3c1f3cfdd152dbb847406f7cd50680de618fdfbb
Yiqian Zhou <yiqian@lightup.ai>
2021-03-31 12:17:41 -0700

    Support RHEL

---
+++ rhel_setup_part_one.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install autossh
+
+source user_config.sh
+source fixed_config.sh
+
+mkdir -p keys
+
+echo "generating new keypair to login to Lightup, name=${LIGHTUP_CONNECT_KEYPAIR_NAME}"
+if [[ ! -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} ]]; then
+  ssh-keygen -t rsa -b 4096 -N "" -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}
+else
+  echo "keypair already exists, skipping..."
+fi
+
+echo; echo "please share the following public key with Lightup (./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}.pub):"; echo
+cat ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}.pub
