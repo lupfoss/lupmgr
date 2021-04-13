e21032ac607b7bc70d29442e340b3d69c457f01b
Manu Bansal <manub@lightup.ai>
2021-04-13 05:07:13 +0000

    cleanup

--- generate_command.sh
+++ generate_command.sh
@@ -2,8 +2,10 @@
 
 source user_config.sh
 source fixed_config.sh
+source utils.sh
 
 this_dir=$(pwd)
+public_ip=$(discover_public_ip)
 
 echo  \
 "AUTOSSH_DEBUG=1 \
@@ -16,6 +18,6 @@ autossh -f -M 0 ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME} -p $
 	-o ServerAliveInterval=30 \
 	-o ServerAliveCountMax=3 \
 	-R ${LIGHTUP_CONNECT_MAPPED_PORT}:localhost:22 \
-	-R 0.0.0.0:${LIGHTUP_DATAPLANE_ADMIN_PORT}:localhost:${LIGHTUP_DATAPLANE_ADMIN_PORT} \
-	-R 0.0.0.0:${LIGHTUP_DATAPLANE_APP_PORT}:localhost:${LIGHTUP_CONNECT_APP_PORT} \
+	-R 0.0.0.0:${LIGHTUP_DATAPLANE_ADMIN_PORT}:${public_ip}:${LIGHTUP_DATAPLANE_ADMIN_PORT} \
+	-R 0.0.0.0:${LIGHTUP_DATAPLANE_APP_PORT}:${public_ip}:${LIGHTUP_DATAPLANE_APP_PORT} \
 	-vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}"
