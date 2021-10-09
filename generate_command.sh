57af16059019f13b69175a8bd0b65b86039ea572
Ming Zhang <ming@lightup.ai>
2021-10-08 18:06:39 -0700

    establish db tunnel to install process for db tunnel case

--- generate_command.sh
+++ generate_command.sh
@@ -7,6 +7,18 @@ source utils.sh
 this_dir=$(pwd)
 private_ip=$(discover_private_ip)
 
+reverse_port_list="-R ${LIGHTUP_CONNECT_MAPPED_PORT}:localhost:22 "
+if [[ $INSTALL_DATAPLANE = "1" ]]
+then
+	reverse_port_list="${reverse_port_list} \
+	-R 0.0.0.0:${LIGHTUP_DATAPLANE_ADMIN_PORT}:${private_ip}:${LIGHTUP_DATAPLANE_ADMIN_PORT} \
+	-R 0.0.0.0:${LIGHTUP_DATAPLANE_APP_PORT}:${private_ip}:${LIGHTUP_DATAPLANE_APP_PORT} \
+	-R 0.0.0.0:${LIGHTUP_DATAPLANE_K8S_PORT}:${private_ip}:${LIGHTUP_DATAPLANE_K8S_PORT}"
+else
+	reverse_port_list="${reverse_port_list} \
+	-R 0.0.0.0:${DB_PORT}:${DB_HOST}:${DB_PORT}"
+fi
+
 echo  \
 "AUTOSSH_DEBUG=1 \
 AUTOSSH_LOGFILE=${this_dir}/autossh.log \
@@ -17,8 +29,5 @@ autossh -f -M 0 ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME} -p $
 	-o StrictHostKeyChecking=no \
 	-o ServerAliveInterval=30 \
 	-o ServerAliveCountMax=3 \
-	-R ${LIGHTUP_CONNECT_MAPPED_PORT}:localhost:22 \
-	-R 0.0.0.0:${LIGHTUP_DATAPLANE_ADMIN_PORT}:${private_ip}:${LIGHTUP_DATAPLANE_ADMIN_PORT} \
-	-R 0.0.0.0:${LIGHTUP_DATAPLANE_APP_PORT}:${private_ip}:${LIGHTUP_DATAPLANE_APP_PORT} \
-	-R 0.0.0.0:${LIGHTUP_DATAPLANE_K8S_PORT}:${private_ip}:${LIGHTUP_DATAPLANE_K8S_PORT} \
+	${reverse_port_list} \
 	-vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}"
