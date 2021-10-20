e4a9e146c5f5e7d027ab402ecf58721d498908ec
jeff-lightup <jeff@lightup.ai>
2021-10-20 16:33:40 -0700

    Host specific SSH key name / Install only with no DB tunnel (#20)
    - Add hostname and port number to the ssh key name
    - Don't configure DB tunnel mode if either the port or host are missing
    
    Tested with:
    
    No install:
    ```
    LIGHTUP_INSTALL=0 CONNECT_PORT=9002 LIGHTUP_TLA=jefftest LIGHTUP_TOKEN=XYZ ./bootstrap.sh
    ```
    No install but with a fake DB tunnel:
    ```
    DB_PORT=9010 DB_HOST=localhost LIGHTUP_INSTALL=0 CONNECT_PORT=9002 LIGHTUP_TLA=jefftest LIGHTUP_TOKEN=XYZ ./bootstrap.sh
    ```
    And setup a second VM and again tested with no install:
    ```
    LIGHTUP_INSTALL=0 CONNECT_PORT=9003 LIGHTUP_TLA=jefftest LIGHTUP_TOKEN=XYZ ./bootstrap.sh
    ```
    For both VMs I made sure that we could ssh from the connect VM.

--- generate_command.sh
+++ generate_command.sh
@@ -14,21 +14,28 @@ then
 	-R 0.0.0.0:${LIGHTUP_DATAPLANE_APP_PORT}:${private_ip}:${LIGHTUP_DATAPLANE_APP_PORT} \
 	-R 0.0.0.0:${LIGHTUP_DATAPLANE_K8S_PORT}:${private_ip}:${LIGHTUP_DATAPLANE_K8S_PORT}"
 else
-	reverse_port_list="-R 0.0.0.0:${DB_PORT}:${DB_HOST}:${DB_PORT}"
+	if [ -n "$DB_PORT" ] && [ -n "$DB_HOST" ]
+	then
+		reverse_port_list="-R 0.0.0.0:${DB_PORT}:${DB_HOST}:${DB_PORT}"
+	fi
 fi
 
-echo  \
-"AUTOSSH_DEBUG=1 \
-AUTOSSH_LOGFILE=${this_dir}/autossh-dataplane.log \
-AUTOSSH_PIDFILE=${this_dir}/autossh-dataplane.pid \
-autossh -f -M 0 ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME} -p ${LIGHTUP_CONNECT_SERVER_PORT} -N \
-	-o ExitOnForwardFailure=yes \
-	-o UserKnownHostsFile=/dev/null \
-	-o StrictHostKeyChecking=no \
-	-o ServerAliveInterval=30 \
-	-o ServerAliveCountMax=3 \
-	${reverse_port_list} \
-	-vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}"
+if [ -n "$reverse_port_list" ]
+then
+	echo  \
+	"AUTOSSH_DEBUG=1 \
+	AUTOSSH_LOGFILE=${this_dir}/autossh-dataplane.log \
+	AUTOSSH_PIDFILE=${this_dir}/autossh-dataplane.pid \
+	autossh -f -M 0 ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME} -p ${LIGHTUP_CONNECT_SERVER_PORT} -N \
+		-o ExitOnForwardFailure=yes \
+		-o UserKnownHostsFile=/dev/null \
+		-o StrictHostKeyChecking=no \
+		-o ServerAliveInterval=30 \
+		-o ServerAliveCountMax=3 \
+		${reverse_port_list} \
+		-vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}"
+fi
+
 echo  \
 "AUTOSSH_DEBUG=1 \
 AUTOSSH_LOGFILE=${this_dir}/autossh-connect.log \
