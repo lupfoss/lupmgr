8442a190473d153dcb3056a959d3553a40517351
Jeff Merrick <jeff@lightup.ai>
2021-10-27 14:38:05 -0700

    wip: Split bootstrap into individual operations

---
+++ connect_ssh_to_lightup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+source init.sh
+source user_config.sh
+source fixed_config.sh
+
+this_dir=$(pwd)
+
+mkdir -p generated/
+sudo cp lightup-ssh-connect.service /etc/systemd/system/lightup-ssh-connect.service
+
+echo "#!/usr/bin/env bash" > generated/lightup-ssh-connect-service.sh
+echo  \
+"AUTOSSH_DEBUG=1 \
+AUTOSSH_LOGFILE=${this_dir}/autossh-connect.log \
+AUTOSSH_PIDFILE=${this_dir}/autossh-connect.pid \
+autossh -f -M 0 ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME} -p ${LIGHTUP_CONNECT_SERVER_PORT} -N \
+    -o ExitOnForwardFailure=yes \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    -o ServerAliveInterval=30 \
+    -o ServerAliveCountMax=3 \
+    -R ${LIGHTUP_CONNECT_MAPPED_PORT}:localhost:22 \
+    -vvv -i $this_dir/keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}" >> generated/lightup-ssh-connect-service.sh
+
+sudo cp generated/lightup-ssh-connect-service.sh /opt/lightup/lightup-ssh-connect-service.sh
+sudo chmod +x /opt/lightup/lightup-ssh-connect-service.sh
+
+sudo systemctl enable lightup-ssh-connect
+sudo systemctl start lightup-ssh-connect.service
+#sudo systemctl status lightup-ssh-connect.service || true
---
+++ init.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+TOK=${LIGHTUP_TOKEN}
+BRANCH=${LIGHTUP_BRANCH:-main}
+INSTALL_DATAPLANE="${LIGHTUP_INSTALL:-1}"
+LIGHTUP_CONNECT_MAPPED_PORT="${CONNECT_PORT:-9000}"
+LIGHTUP_CONNECT_KEYPAIR_NAME="${LIGHTUP_TLA}-to-lightup-${HOSTNAME}-${LIGHTUP_CONNECT_MAPPED_PORT}"
+
+
+# determine distro
+
+if [[ -f /etc/lsb-release && $(grep -Eo 'DISTRIB_ID=Ubuntu' /etc/lsb-release) ]]; then
+    DISTRO="Ubuntu"
+fi
+
+if [[ -f /etc/redhat-release && $(grep -Eo 'release 7\.' /etc/redhat-release) ]]; then
+    DISTRO="RHEL7"
+fi
+
+if [[ -f /etc/redhat-release && $(grep -Eo 'release 8\.' /etc/redhat-release) ]]; then
+    DISTRO="RHEL8"
+fi
+
+if [[ ${DISTRO-} = "" ]]; then
+    echo "error: distro not supported, please see list of supported platforms at: "
+    echo "https://docs.lightup.ai/lightup-quickstart/deployment/lightup-hybrid-quick-start#create-a-vm"
+    exit 1
+fi
+
+# install autossh with distro-specific commands
+
+if [[ $DISTRO = "Ubuntu" ]]; then
+    sudo apt update
+
+    #the following is needed because it looks like the replicated
+    #installer is corrupting openssh packages and sending apt in a bad
+    #state, so this script fails if run again after replicated unless
+    #the following is executed.
+    sudo apt --fix-broken install
+
+    sudo apt install -y autossh sshpass git
+fi
+
+if [[ $DISTRO = "RHEL7" ]]; then
+    sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+    sudo yum upgrade -y
+    sudo yum install -y autossh sshpass git
+fi
+
+if [[ $DISTRO = "RHEL8" ]]; then
+    sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+    sudo yum upgrade -y
+    sudo yum install -y autossh sshpass git
+fi
+
+# make user sudo passwordless to enable script runs
+NAME=$(whoami)
+if [[ ! -f lup-${NAME} ]]; then
+    sudo echo "${NAME} ALL=(ALL) NOPASSWD: ALL" > lup-${NAME}
+    sudo chmod 440 lup-${NAME}
+    sudo chown root:root lup-${NAME}
+    sudo cp lup-${NAME} /etc/sudoers.d/
+fi
+
+[[ -d lupmgr ]] || git clone https://github.com/lupfoss/lupmgr.git
+cd lupmgr && git pull && git checkout ${BRANCH}
+
+echo "export LIGHTUP_TLA=${LIGHTUP_TLA}" > user_config.sh
+echo "export INSTALL_DATAPLANE=${INSTALL_DATAPLANE}" >> user_config.sh
+echo "export LIGHTUP_CONNECT_KEYPAIR_NAME=${LIGHTUP_CONNECT_KEYPAIR_NAME}" >> user_config.sh
+echo "export LIGHTUP_CONNECT_MAPPED_PORT=${LIGHTUP_CONNECT_MAPPED_PORT}" >> user_config.sh
+echo "export LIGHTUP_DATAPLANE_USERNAME=$(whoami)" >> user_config.sh
+echo "export LIGHTUP_DATAPLANE_LUPMGR_DIR=$(pwd)" >> user_config.sh
+echo "export LIGHTUP_DATAPLANE_HOMEDIR=${HOME}" >> user_config.sh
+source user_config.sh
+source fixed_config.sh
+source utils.sh
+
+#----
+
+mkdir -p keys
+
+echo "generating new keypair to login to Lightup, name=${LIGHTUP_CONNECT_KEYPAIR_NAME}"
+if [[ ! -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} ]]; then
+  ssh-keygen -t rsa -b 4096 -N "" -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}
+  sshpass -p ${TOK} ssh-copy-id -i ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}.pub -o StrictHostKeyChecking=no ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME}
+else
+  echo "keypair already exists, skipping..."
+fi
+
+# key setup
+echo "adding Lightup's public key to authorized keys..."
+scp -o "StrictHostKeyChecking no" -i ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} -P ${LIGHTUP_CONNECT_SERVER_PORT} ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME}:~/"${LIGHTUP_ACCEPT_KEYPAIR_NAME}.pub" ./keys/
+mkdir -p ~/.ssh && cat "./keys/${LIGHTUP_ACCEPT_KEYPAIR_NAME}.pub" >> ~/.ssh/authorized_keys
