89c4b66307e0b04ea87ad8c27565de5b91d228ec
Jeff Merrick <jeff@lightup.ai>
2021-10-27 18:27:29 -0700

    cleanup

--- disconnect.sh
+++ disconnect.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
 echo "Stopping lightup-ssh-connect service..."
 sudo systemctl stop lightup-ssh-connect.service
 echo "Stopping lightup-dataplane-connect service..."
--- uninstall.sh
+++ uninstall.sh
@@ -6,12 +6,16 @@ echo "Uninstalling lightup-ssh-connect..."
 sudo rm -rf lightup-ssh-connect.service.pid
 sudo rm -rf lightup-ssh-connect.service.log
 sudo systemctl disable lightup-ssh-connect
+sudo rm -rf /etc/systemd/system/lightup-ssh-connect.service
 
 echo "Uninstalling lightup-dataplane-connect..."
 sudo rm -rf lightup-dataplane-connect.service.log
 sudo rm -rf lightup-dataplane-connect.service.pid
 sudo systemctl disable lightup-dataplane-connect
+sudo rm -rf /etc/systemd/system/lightup-dataplane-connect.service
 
 sudo rm -rf /opt/lightup/*
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
 
 echo "Lightup uninstalled"
