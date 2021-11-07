9e2b1000a3599c50cfe23ee1c554cd8a591c57c1
Matt O'Donnell <matt@absolutod.com>
2021-11-07 10:51:11 -0500

    * added more debugging

--- init.sh
+++ init.sh
@@ -95,11 +95,12 @@ mkdir -p keys
 
 echo "generating new keypair to login to Lightup, name=${LIGHTUP_CONNECT_KEYPAIR_NAME}"
 if [[ ! -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} && $DISTRO = "AL2" ]]; then
-  echo "Using expect to copy key over"
+  echo "Using EXPECT to copy the key over"
   ssh-keygen -t rsa -b 4096 -N "" -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}
   ./copy-public-key.sh ${LIGHTUP_CONNECT_USER_NAME} ${LIGHTUP_CONNECT_SERVER_NAME} ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}.pub ${TOK}
 
 elif [[ ! -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} ]]; then
+  echo "Using SSHPASS to copy the key over"
   ssh-keygen -t rsa -b 4096 -N "" -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}
   sshpass -p ${TOK} ssh-copy-id -i ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}.pub -o StrictHostKeyChecking=no ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME}
 
