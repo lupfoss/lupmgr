c89d4a9039636edbbfda33223e33d034c74ba6bb
Matt O'Donnell <matt@absolutod.com>
2021-11-09 16:02:29 -0500

    * changed the touched file to denote things have been initialized.

--- init.sh
+++ init.sh
@@ -34,7 +34,7 @@ fi
 
 # install autossh with distro-specific commands
 
-if [[ ! -f first_run.txt && $DISTRO = "Ubuntu" ]]; then
+if [[ ! -f initialized.txt && $DISTRO = "Ubuntu" ]]; then
     sudo apt update
 
     #the following is needed because it looks like the replicated
@@ -46,19 +46,19 @@ if [[ ! -f first_run.txt && $DISTRO = "Ubuntu" ]]; then
     sudo apt install -y autossh sshpass git
 fi
 
-if [[ ! -f first_run.txt && $DISTRO = "RHEL7" ]]; then
+if [[ ! -f initialized.txt && $DISTRO = "RHEL7" ]]; then
     rpm -qa | grep epel-release-7 || sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
     sudo yum upgrade -y
     sudo yum install -y autossh sshpass git
 fi
 
-if [[ ! -f first_run.txt && $DISTRO = "RHEL8" ]]; then
+if [[ ! -f initialized.txt && $DISTRO = "RHEL8" ]]; then
     rpm -qa | grep epel-release-8 || sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
     sudo yum upgrade -y
     sudo yum install -y autossh sshpass git
 fi
 
-if [[ ! -f first_run.txt && $DISTRO = "AL2" ]]; then
+if [[ ! -f initialized.txt && $DISTRO = "AL2" ]]; then
     echo "Detected Amazon Linux 2"
     sudo yum upgrade -y
     sudo yum install -y expect git
@@ -66,7 +66,7 @@ fi
 
 # make user sudo passwordless to enable script runs
 NAME=$(whoami)
-if [[ ! -f first_run.txt && ! -f lup-${NAME} ]]; then
+if [[ ! -f initialized.txt && ! -f lup-${NAME} ]]; then
     sudo echo "${NAME} ALL=(ALL) NOPASSWD: ALL" > lup-${NAME}
     sudo chmod 440 lup-${NAME}
     sudo chown root:root lup-${NAME}
@@ -76,7 +76,7 @@ fi
 [[ -d lupmgr ]] || git clone https://github.com/lupfoss/lupmgr.git
 cd lupmgr
 
-if [[ ! -f first_run.txt ]]; then
+if [[ ! -f initialized.txt ]]; then
     git pull && git checkout ${BRANCH}
 fi
 
@@ -88,7 +88,7 @@ fi
 #echo "export LIGHTUP_DATAPLANE_LUPMGR_DIR=$(pwd)" >> user_config.sh
 #echo "export LIGHTUP_DATAPLANE_HOMEDIR=${HOME}" >> user_config.sh
 
-if [[ ! -f ./first_run.txt && ! -f ./user_config.sh ]]; then
+if [[ ! -f ./initialized.txt && ! -f ./user_config.sh ]]; then
     echo "Generating the user_config.sh"
     source create-user-config.sh
 else
@@ -104,7 +104,7 @@ source utils.sh
 mkdir -p keys
 
 echo "generating new keypair to login to Lightup, name=${LIGHTUP_CONNECT_KEYPAIR_NAME}"
-if [[ ! -f first_run.txt && ! -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} ]]; then
+if [[ ! -f initialized.txt && ! -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} ]]; then
     ssh-keygen -t rsa -b 4096 -N "" -f ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}
 
 else
@@ -112,7 +112,7 @@ else
 fi
 
 echo "Copying public key over to control plane"
-if [[ ! -f first_run.txt &&  && $DISTRO = "AL2" ]]; then
+if [[ ! -f initialized.txt &&  && $DISTRO = "AL2" ]]; then
     echo "Using EXPECT to copy the key over"
     expect <<'END_EXPECT'
           spawn ssh-copy-id -i $env(HOME)/lupmgr/keys/$env(LIGHTUP_CONNECT_KEYPAIR_NAME) $env(LIGHTUP_CONNECT_USER_NAME)@$env(LIGHTUP_CONNECT_SERVER_NAME) -o StrictHostKeyChecking=no
@@ -121,7 +121,7 @@ if [[ ! -f first_run.txt &&  && $DISTRO = "AL2" ]]; then
           expect eof
 END_EXPECT
 
-elif [[ ! -f first_run.txt ]]
+elif [[ ! -f initialized.txt ]]
   echo "Using SSHPASS to copy the key over"
   sshpass -p ${TOK} ssh-copy-id -i ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME}.pub -o StrictHostKeyChecking=no ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME}
 
@@ -131,7 +131,7 @@ fi
 
 
 # key setup
-if [[ ! -f first_run.txt ]]
+if [[ ! -f initialized.txt ]]
     echo "adding Lightup's public key to authorized keys..."
     scp -o "StrictHostKeyChecking no" -i ./keys/${LIGHTUP_CONNECT_KEYPAIR_NAME} -P ${LIGHTUP_CONNECT_SERVER_PORT} ${LIGHTUP_CONNECT_USER_NAME}@${LIGHTUP_CONNECT_SERVER_NAME}:~/"${LIGHTUP_ACCEPT_KEYPAIR_NAME}.pub" ./keys/
     mkdir -p ~/.ssh && cat "./keys/${LIGHTUP_ACCEPT_KEYPAIR_NAME}.pub" >> ~/.ssh/authorized_keys
@@ -141,9 +141,9 @@ else
 
 fi
 
-if [[ ! -f first_run.txt ]]; then
+if [[ ! -f initialized.txt ]]; then
     echo "This is the first time initializing"
-    touch first_run.txt
+    touch initialized.txt
 else
     echo "We have already run, skip"
 fi
