83bb8a3c7f721c8682eeb6a1624da444652508c8
Matt O'Donnell <matt@absolutod.com>
2021-11-10 21:21:54 -0500

    * broke out the user_config.sh creation * update the connects_* to use the new script * updated init.sh to also use the new script

--- connect_dataplane_to_lightup.sh
+++ connect_dataplane_to_lightup.sh
@@ -7,14 +7,8 @@ if systemctl is-active --quiet lightup-dataplane-connect ; then
     exit 1
 fi
 
-if [ -f init.sh ]; then
-    source init.sh
-else
-    # outside of repo - download init.sh
-    BRANCH=${LIGHTUP_BRANCH:-main}
-    curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/lupfoss/lupmgr/$BRANCH/init.sh > init.sh
-    source init.sh
-    rm ../init.sh  # cleanup downloaded init.sh
+if [ ! -f user_config.sh ]; then
+    source create-user-config.sh
 fi
 
 source user_config.sh
