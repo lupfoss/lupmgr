a67d577dde6f18ddd6e300e66f6ce68563f68801
Matt O'Donnell <matt@absolutod.com>
2021-11-11 16:02:18 -0500

    * added missing exports

--- create-user-config.sh
+++ create-user-config.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "export LIGHTUP_TLA=${LIGHTUP_TLA}" > user_config.sh
+echo "export HA_INSTALL=${HA_INSTALL}" >> user_config.sh
 echo "export INSTALL_DATAPLANE=${INSTALL_DATAPLANE}" >> user_config.sh
+echo "export LIGHTUP_CONNECT_ADMIN_PORT=${LIGHTUP_CONNECT_ADMIN_PORT}" >> user_config.sh
+echo "export LIGHTUP_CONNECT_APP_PORT=${LIGHTUP_CONNECT_APP_PORT}" >> user_config.sh
 echo "export LIGHTUP_CONNECT_KEYPAIR_NAME=${LIGHTUP_CONNECT_KEYPAIR_NAME}" >> user_config.sh
 echo "export LIGHTUP_CONNECT_MAPPED_PORT=${LIGHTUP_CONNECT_MAPPED_PORT}" >> user_config.sh
 echo "export LIGHTUP_DATAPLANE_USERNAME=$(whoami)" >> user_config.sh
